8183aab19d84cce227e24b6115347f19cd1ab9c8
josenunezapps <josene242@gmail.com>
2026-09-24 18:28:06 -0300

    Compactar accesos rápidos de Zenix Agent

--- assets/chatbot.js
+++ assets/chatbot.js
@@ -55,9 +55,9 @@
 
       <div class="zenix-chat-quick" aria-label="Opciones rápidas">
         <button type="button" data-message="Necesito una página web">Web</button>
-        <button type="button" data-message="Necesito una app Android">App Android</button>
+        <button type="button" data-message="Necesito una app Android">App</button>
         <button type="button" data-message="Quiero automatizar mi negocio">Automatizar</button>
-        <button type="button" data-message="Tengo una idea y quiero desarrollarla">Tengo una idea</button>
+        <button type="button" data-message="Tengo una idea y quiero desarrollarla">Idea</button>
         <button type="button" data-message="Quiero pedir un presupuesto">Presupuesto</button>
       </div>
 
